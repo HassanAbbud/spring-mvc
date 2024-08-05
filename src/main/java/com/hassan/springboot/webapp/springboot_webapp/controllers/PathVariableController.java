f3b2519e621c163c12d126117d5ae0ba1ac8abb8
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-05 10:12:21 -0600

    @Value, property injection & encoding charset

--- src/main/java/com/hassan/springboot/webapp/springboot_webapp/controllers/PathVariableController.java
+++ src/main/java/com/hassan/springboot/webapp/springboot_webapp/controllers/PathVariableController.java
@@ -9,6 +9,7 @@ import com.hassan.springboot.webapp.springboot_webapp.models.dto.ParamDto;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+    @Value("${config.code}")
+    private Integer code;
+
+    @Value("${config.name}")
+    private String name;
+
+    @Value("${config.message}")
+    private String message;
+
+    @Value("${config.listOfValues}")
+    private String[] list;
+    
+    // @Value("#{${config.listOfValues.toUpperCase()}}")
+    // private String listUpper;
+
+    @Value("${config.specialCharacters}")
+    private String specialCharacters;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
@@ -42,6 +60,18 @@ public class PathVariableController {
         
         user.setName(user.getName().toUpperCase());
         return user;
+    }
+    
+    @GetMapping("values")
+    public Map<String, Object> getConfigValues() {
+        Map<String, Object> json = new HashMap<>() ;
+        json.put("code", code);
+        json.put("usename", name);
+        json.put("list", list);
+        json.put("message", message);
+        json.put("specialCharacters", specialCharacters);
+        //json.put("listUpperCase", listUpper);
+        return json;
     }
     
     

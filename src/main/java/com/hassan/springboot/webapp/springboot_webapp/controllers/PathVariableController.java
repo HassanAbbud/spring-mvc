acb9127b8675cbf250a1923f56ffa72372395b05
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-05 10:53:58 -0600

    Environment objects to read .properties configurations

--- src/main/java/com/hassan/springboot/webapp/springboot_webapp/controllers/PathVariableController.java
+++ src/main/java/com/hassan/springboot/webapp/springboot_webapp/controllers/PathVariableController.java
@@ -10,7 +10,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+    //Get individual config properties
     @Value("${config.code}")
     private Integer code;
 
@@ -46,6 +49,9 @@ public class PathVariableController {
     
     @Value("#{${config.valuesMap}.product}")
     private String valuesMapProduct;
+
+    @Autowired
+    private Environment environment; 
 
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
@@ -84,6 +90,7 @@ public class PathVariableController {
         json.put("items", valuesMap);
         json.put("itemsPrice", valuesMapPrice);
         json.put("itemsProduct", valuesMapProduct);
+        json.put("codeEnv", environment.getProperty("config.code", Long.class));
         return json;
     }
     
